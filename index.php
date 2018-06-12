3d52c76f9521cf4d10807e69d8f1d3e3a813ac41
shenyan <52o@qq52o.cn>
2018-06-12 08:58:19 +0800

    入口文件

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 入口文件
+ * Created by PhpStorm.
+ * User: ShenYan
+ * Date: 2018/6/9
+ * Time: 15:03
+ */
+
+//根目录
+define('IHAD',realpath('/'));
+//核心文件
+define('CORE',IHAD.'/core');
+//调试
+define('DEBUG',true);
+
+if(DEBUG) {
+    ini_set('display_error','On');
+} else {
+    ini_set('display_error','Off');
+}
