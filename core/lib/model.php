2ce177fd2fb790fa58827d72e2e2da6e5bc4925d
shenyan <52o@qq52o.cn>
2018-06-15 09:24:06 +0800

    数据库model

---
+++ core/lib/model.php
@@ -0,0 +1,15 @@
+<?php
+namespace  core\lib;
+use core\lib\config;
+class model extends \PDO
+{
+    public function __construct()
+    {
+        $database = config::all('database');
+        try{
+            parent::__construct($database['DSN'], $database['USERNAME'], $database['PASSWD']);
+        } catch (\PDOException $e){
+            p($e->getMessage());
+        }
+    }
+}
