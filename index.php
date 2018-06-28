67bc82324d2d8271af7e79a2746d06e2b2c399b9
shenyan <52o@qq52o.cn>
2018-06-28 09:02:44 +0800

    增加composer

--- index.php
+++ index.php
@@ -6,19 +6,25 @@
  * Date: 2018/6/9
  * Time: 15:03
  */
-
 //根目录
 define('IHAD',realpath('./'));
 //核心文件
 define('CORE',IHAD.'/core');
 //项目目录
 define('APP',IHAD.'/app');
-
 define('MODULE','app');
 //调试
 define('DEBUG',true);
+//引入composer
+include  "vendor/autoload.php";
 
 if(DEBUG) {
+    $whoops = new \Whoops\Run;
+    $errorTitle = '你又在写Bug啦~~~';
+    $option = new \Whoops\Handler\PrettyPageHandler;
+    $option->setPageTitle($errorTitle);
+    $whoops->pushHandler($option);
+    $whoops->register();
     ini_set('display_error','On');
 } else {
     ini_set('display_error','Off');
