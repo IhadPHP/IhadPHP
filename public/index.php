27c29fbfe5310fb6d42a111dcdf1671ad230dd95
shenyan <52o@qq52o.cn>
2018-07-05 09:28:54 +0800

    修改入口文件

--- public/index.php
+++ public/index.php
@@ -7,33 +7,7 @@
  * Time: 15:03
  */
 //根目录
-define('IHAD',realpath('./'));
-//核心文件
-define('CORE',IHAD.'/core');
-//项目目录
-define('APP',IHAD.'/app');
-define('MODULE','app');
-//调试
-define('DEBUG',true);
-//引入composer
-include  "vendor/autoload.php";
+define('IHAD',realpath('../'));
 
-if(DEBUG) {
-    $whoops = new \Whoops\Run;
-    $errorTitle = '你又在写Bug啦~~~';
-    $option = new \Whoops\Handler\PrettyPageHandler;
-    $option->setPageTitle($errorTitle);
-    $whoops->pushHandler($option);
-    $whoops->register();
-    ini_set('display_error','On');
-} else {
-    ini_set('display_error','Off');
-}
-
-//函数库
-include CORE.'/common/function.php';
-include CORE.'/ihad.php';
-//自动加载
-spl_autoload_register('\core\ihad::load');
-
-\core\ihad::run();
+// 加载框架引导文件
+require IHAD . '/core/start.php';
